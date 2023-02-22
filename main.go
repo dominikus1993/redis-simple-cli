276565a3d1439835d5da31286f9df195ea6ad895
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2023-02-22 14:54:30 +0000

    .

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"log"
+	"os"
+	"sort"
 	"time"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/urfave/cli/v2"
 )
 
 func NewRedisClient(ctx context.Context, addr string) (*redis.Client, error) {
@@ -27,5 +32,27 @@ func NewRedisClient(ctx context.Context, addr string) (*redis.Client, error) {
 }
 
 func main() {
-
+	app := &cli.App{
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "redis",
+				Usage:    "redis",
+				Required: true,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			redisConnection := ctx.String("redis")
+			client, err := NewRedisClient(ctx.Context, redisConnection)
+			if err != nil {
+				return cli.Exit(errors.Join(err, errors.New("can't connect to redis")), 1)
+			}
+			client.Scan(ctx.Context)
+			return cli.Exit("da", 32)
+		},
+	}
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
+	if err := app.Run(os.Args); err != nil {
+		log.Fatalln(err)
+	}
 }
